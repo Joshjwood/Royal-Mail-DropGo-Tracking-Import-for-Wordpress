4e3721fe6abc917d7fb3d3309b8daccaa3dbece0
Josh <joshjwood91@gmail.com>
2023-06-21 15:32:26 +0100

    init

--- main.py
+++ main.py
@@ -3,12 +3,4 @@ from classes import *
 bot = RoyalMailTrackingImporter()
 
 bot.GetTrackingIDs()
-#input("Check the listed postcodes for issues and press Enter to proceed:\n")
 bot.InputTrackingIDs()
-
-#
-# with open("rm_post_dict.json", "r") as postcodes:
-#     data = json.load(postcodes)
-#
-#     for i in data:
-#         print(data[i])
